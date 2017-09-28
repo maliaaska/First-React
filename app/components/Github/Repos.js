844178283ac0a5e9f9759b951b4502a87c0038a9
Arkadiusz <asmilewski@gmail.com>
2017-09-28 14:05:09 +0200

    Profile, repos, notes

---
+++ app/components/Github/Repos.js
@@ -0,0 +1,16 @@
+var React = require ('react');
+
+
+var Repos = React.createClass  ({
+    render: function() {
+        return (
+            <div> 
+                <p> REPOS:</p>
+                <p>{this.props.repos}</p>
+
+            </div>
+        )
+    }
+});
+
+module.exports = Repos;
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,25 +1,35 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
+
 
 var Profile = React.createClass ({
     getInitialState: function() {
         return {
-            notes: [],
-            bio: {},
-            repos: []
+            notes: ["bla"], 
+            bio: {
+                name: 'Arkadiusz Milewski'
+            },
+            repos: [2,3,4,5]
         }
     },
     render : function() {
+        console.log(this.props)
         return (
            <div className="row">
                <div className="col-md-4">
-                   User Profile Component
+                   <UserProfile username={this.props.params.username} bio={this.state.bio} />
+                   
                </div>
                <div className="col-md-4">
-                   Repos Component
+                   < Repos repos={this.state.repos}/>
+                   
                </div>
                <div className="col-md-4">
-                   Note Component
+                   <Notes notes={this.state.notes}/>
+                   
                </div>
            </div> 
         )
